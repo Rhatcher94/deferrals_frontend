5a3c25efe71c7a56852d5c0ed72fb2740937448c
Your name <Your Email address>
2022-01-08 23:15:47 -0500

    Updates

--- src/helpers/UserLevels.js
+++
@@ -1,7 +0,0 @@
-const UserLevels = {
-    SUPER_ADMIN: 1,
-    ORG_ADMIN: 2,
-    USER: 3
-}
-
-export default UserLevels
--- src/stores.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const showMessage = writable({show: false, message: "", type: ""});
-export const invoices = writable([]);
-export const selectedInvoice = writable([]);
-export const user = writable({});
-export const myOrganization = writable({});
